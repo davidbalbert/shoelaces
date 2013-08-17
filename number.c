d28910e9f913cf8757262da2ade1bcd04027473b
David Albert <davidbalbert@gmail.com>
2013-08-17 00:22:30 -0400

    Use new type naming convention

--- number.c
+++ number.c
@@ -1,16 +1,16 @@
 #include "shoelaces.h"
 
-sl_value sl_Integer;
+sl_value sl_tInteger;
 
 sl_value sl_new_integer(int i)
 {
         sl_value n = sl_alloc(struct SLInteger);
-        SL_BASIC(n)->type = sl_Integer;
+        SL_BASIC(n)->type = sl_tInteger;
         SL_INTEGER(n)->value = i;
         return n;
 }
 
 void sl_init_number()
 {
-        sl_Integer = sl_new_type("Integer");
+        sl_tInteger = sl_new_type("Integer");
 }
--- type.c
+++ type.c
@@ -1,17 +1,17 @@
 #include "shoelaces.h"
 
-sl_value sl_Type;
+sl_value sl_tType;
 
 sl_value sl_new_type(char *name)
 {
         sl_value t = sl_alloc(struct SLType);
-        SL_BASIC(t)->type = sl_Type;
+        SL_BASIC(t)->type = sl_tType;
         SL_TYPE(t)->name = name;
         return t;
 }
 
 void sl_init_type()
 {
-        sl_Type = sl_new_type("Type");
-        SL_BASIC(sl_Type)->type = sl_Type;
+        sl_tType = sl_new_type("Type");
+        SL_BASIC(sl_tType)->type = sl_tType;
 }
